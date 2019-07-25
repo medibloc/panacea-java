90739b247d62beefe3ef538c18a13c35e89bb7d4
Cha Minkyoo <minkyoo.cha@medibloc.org>
2019-07-25 14:17:03 +0900

    Modify variable names

--- src/main/java/org/medibloc/panacea/api/client/domain/Record.java
+++ src/main/java/org/medibloc/panacea/api/client/domain/Record.java
@@ -7,22 +7,30 @@ import lombok.Setter;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
+import java.util.Date;
+
 @Getter @Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Record {
     private byte[] key;
     private byte[] value;
-    private String timestamp;
+    @JsonProperty("nano_timestamp")
+    private Long nanoTimestamp;
     @JsonProperty("writer_address")
     private String writerAddress;
+
+    public Date getTime() {
+        return new Date(nanoTimestamp/1000000);
+    }
 
     @Override
     public String toString() {
         return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
                 .append("key", key)
                 .append("value", value)
-                .append("timestamp", timestamp)
+                .append("nanoTimestamp", nanoTimestamp)
                 .append("writer", writerAddress)
+                .append("date", getTime())
                 .toString();
     }
 }
