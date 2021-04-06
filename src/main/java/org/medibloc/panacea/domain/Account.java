74bc15f59df916b6107441544af2fbd9aed05935
Youngjoon Lee <yjlee@medibloc.org>
2021-04-06 17:53:40 +0900

    comment

--- src/main/java/org/medibloc/panacea/domain/Account.java
+++ src/main/java/org/medibloc/panacea/domain/Account.java
@@ -25,6 +25,8 @@ public class Account {
         private Long sequence;
         private List<Coin> coins;
 
+        // When the account doesn't exist yet on the blockchain, the following empty response is returned from blockchain.
+        // {"height":"123","result":{"type":"cosmos-sdk/Account","value":{"address":"","coins":[],"public_key":null,"account_number":"0","sequence":"0"}}}
         boolean isEmpty() {
             return this.address.isEmpty() && this.accountNumber == 0 && this.sequence == 0 && this.coins.isEmpty();
         }
