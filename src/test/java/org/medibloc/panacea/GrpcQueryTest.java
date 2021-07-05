72f2fdfc24945871ba707511cf95496a1c568797
gyuguen <gyuguen.jang@medibloc.org>
2021-07-05 14:43:14 +0900

    feat: Add a 'Dockerfile' for integartion testing

--- src/test/java/org/medibloc/panacea/GrpcQueryTest.java
+++ src/test/java/org/medibloc/panacea/GrpcQueryTest.java
@@ -3,12 +3,22 @@ package org.medibloc.panacea;
 import cosmos.auth.v1beta1.BaseAccount;
 import cosmos.base.abci.v1beta1.TxResponse;
 import cosmos.base.v1beta1.Coin;
+import cosmos.tx.v1beta1.BroadcastMode;
+import cosmos.tx.v1beta1.BroadcastTxRequest;
+import cosmos.tx.v1beta1.Fee;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.medibloc.panacea.domain.Coins;
+import org.medibloc.panacea.domain.Transactions;
 import org.medibloc.panacea.utils.CryptoUtils;
+import panacea.aol.v2.MsgCreateTopic;
 import tendermint.p2p.DefaultNodeInfo;
 import tendermint.types.Block;
 
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 public class GrpcQueryTest extends AbstractGrpcTest {
@@ -25,7 +35,6 @@ public class GrpcQueryTest extends AbstractGrpcTest {
 
         BaseAccount account2 = client.getAccount(toAddress);
         Assert.assertEquals(toAddress, account2.getAddress());
-        Assert.assertNotNull(CryptoUtils.getPublicKeyFrom(account2));
     }
 
     @Test
@@ -47,20 +56,47 @@ public class GrpcQueryTest extends AbstractGrpcTest {
     }
 
     @Test
-    public void testGetTxResponseByHash() {
-        TxResponse txResponse = client.getTxResponse("30FAB3C12B46C0424D04006557028872D0F2674F8B9E695A722383F3CA2078FB");
+    public void testGetTxResponseByHash() throws PanaceaApiException, IOException, NoSuchAlgorithmException {
+        TxResponse sendTx = simpleSendTx();
+        TxResponse txResponse = client.getTxResponse(sendTx.getTxhash());
         System.out.println(txResponse);
+    }
+
+    private TxResponse simpleSendTx() throws PanaceaApiException, IOException, NoSuchAlgorithmException {
+        Wallet ownerWallet = getWallet(TestConst.ownerMnemonic);
+        String ownerAddress = ownerWallet.getAddress();
+
+        String topicName = RandomStringUtils.randomAlphabetic(10);
+        System.out.printf("Create topic : %s\n", topicName);
+
+        MsgCreateTopic createTopicMsg = MsgCreateTopic.newBuilder()
+                .setTopicName(topicName)
+                .setDescription("test topic")
+                .setOwnerAddress(ownerAddress)
+                .build();
+
+        String memo = "create topic :" + createTopicMsg.getTopicName();
+        Fee fee = Transactions.createFee(Coins.createCoin(TestConst.denom, "1000"), 200000);
+
+        BroadcastTxRequest request = Transactions.createBroadcastTxRequest(
+                getWallet(TestConst.ownerMnemonic),
+                createTopicMsg,
+                memo,
+                fee,
+                BroadcastMode.BROADCAST_MODE_BLOCK);
+
+        return  client.broadcast(request);
     }
 
     @Test
     public void testGetTxResponsesByHeight() {
-        List<TxResponse> txResponses = client.getTxResponsesByHeight(10367);
+        List<TxResponse> txResponses = client.getTxResponsesByHeight(1);
         System.out.println(txResponses);
     }
 
     @Test
     public void testGetBlockByHeight() {
-        Block block = client.getBlockByHeight(10);
+        Block block = client.getBlockByHeight(1);
         System.out.println(block);
     }
 
