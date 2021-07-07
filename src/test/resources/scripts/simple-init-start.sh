a93b46f3f710596d447cf7dc0c2756fd7711d91f
gyuguen <gyuguen.jang@medibloc.org>
2021-07-07 10:50:00 +0900

    fix: typo recipientMnemonic

--- src/test/resources/scripts/simple-init-start.sh
+++ src/test/resources/scripts/simple-init-start.sh
@@ -15,7 +15,7 @@ echo -e "${RECIPIENT_MNEMONIC}\n\n" | panacead keys add recipientAccount -i
 # NOTE: this command lets you set the number of coins. Make sure this account has some coins
 # with the genesis.app_state.staking.params.bond_denom denom.
 panacead add-genesis-account $(panacead keys show validator -a) 100000000000000umed
-panacead add-genesis-account $(panacead keys show toAccount -a) 100000000000000umed
+panacead add-genesis-account $(panacead keys show recipientAccount -a) 100000000000000umed
 
 # Generate the transaction that creates your validator
 panacead gentx validator 1000000000000umed --commission-rate 0.1 --commission-max-rate 0.2 --commission-max-change-rate 0.01  --min-self-delegation 1000000 --chain-id testing
