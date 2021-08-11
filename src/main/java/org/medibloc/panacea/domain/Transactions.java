ff950e2db0034a3b09f018855e9646dd28070eca
Youngjoon Lee <yjlee@medibloc.org>
2021-08-11 11:50:12 +0900

    revert removed methods

--- src/main/java/org/medibloc/panacea/domain/Transactions.java
+++ src/main/java/org/medibloc/panacea/domain/Transactions.java
@@ -16,6 +16,13 @@ import java.util.List;
 
 public class Transactions {
 
+    public static TxBody createTxBody(GeneratedMessageV3 message, String memo) {
+        return TxBody.newBuilder()
+                .addMessages(Any.pack(message, ""))
+                .setMemo(memo)
+                .build();
+    }
+
    public static TxBody createTxBody(List<? extends GeneratedMessageV3> messages, String memo) {
         TxBody.Builder builder = TxBody.newBuilder();
         for (GeneratedMessageV3 message : messages) {
@@ -52,6 +59,18 @@ public class Transactions {
                 .build();
     }
 
+    public static AuthInfo createAutoInfo(Wallet wallet, Coin coin, long gasLimit) {
+        return createAutoInfo(Collections.singletonList(wallet), createFee(coin, gasLimit));
+    }
+
+    public static AuthInfo createAutoInfo(Wallet wallet, Fee fee) {
+        return createAutoInfo(Collections.singletonList(wallet), fee);
+    }
+
+    public static AuthInfo createAutoInfo(List<Wallet> wallets, Coin coin, long gasLimit) {
+        return createAutoInfo(wallets, createFee(coin, gasLimit));
+    }
+
     public static AuthInfo createAutoInfo(List<Wallet> wallets, Fee fee) {
         List<SignerInfo> signerInfos = new ArrayList<SignerInfo>();
         for (Wallet wallet : wallets) {
@@ -85,12 +104,23 @@ public class Transactions {
         return signatures;
     }
 
+    public static Tx createTxWithSign(Wallet wallet, TxBody txBody, AuthInfo authInfo) throws IOException, NoSuchAlgorithmException {
+        return createTxWithSign(Collections.singletonList(wallet), txBody, authInfo);
+    }
+
     public static Tx createTxWithSign(List<Wallet> wallets, TxBody txBody, AuthInfo authInfo) throws IOException, NoSuchAlgorithmException {
         return Tx.newBuilder()
                 .setBody(txBody)
                 .setAuthInfo(authInfo)
                 .addAllSignatures(createSignatures(wallets, txBody, authInfo))
                 .build();
+    }
+
+    public static Tx createTxWithSign(List<Wallet> wallets,
+                                      GeneratedMessageV3 msg,
+                                      String memo,
+                                      Fee fee) throws IOException, NoSuchAlgorithmException {
+        return createTxWithSign(wallets, Collections.singletonList(msg), memo, fee);
     }
 
     public static Tx createTxWithSign(List<Wallet> wallets,
