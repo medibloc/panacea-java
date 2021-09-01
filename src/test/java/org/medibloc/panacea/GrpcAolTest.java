32e4352fbcf24cfecdc7def1679bf985f13afff6
Youngjoon Lee <yjlee@medibloc.org>
2021-09-01 17:18:12 +0900

    no infinite loop

--- src/test/java/org/medibloc/panacea/GrpcAolTest.java
+++ src/test/java/org/medibloc/panacea/GrpcAolTest.java
@@ -8,7 +8,6 @@ import cosmos.tx.v1beta1.BroadcastTxRequest;
 import cosmos.tx.v1beta1.Fee;
 import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
-import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Assert;
@@ -25,7 +24,7 @@ import java.util.List;
 public class GrpcAolTest extends AbstractGrpcTest {
 
     @Test
-    public void testAol() throws IOException, NoSuchAlgorithmException, PanaceaApiException, InterruptedException, DecoderException {
+    public void testAol() throws Exception {
         Wallet ownerWallet = getWallet(TestConst.ownerMnemonic);
         String ownerAddress = ownerWallet.getAddress();
         Wallet toWallet = getWallet(TestConst.toMnemonic);
@@ -124,7 +123,7 @@ public class GrpcAolTest extends AbstractGrpcTest {
         Assert.assertEquals(0, response.getCode());
     }
 
-    private void testAddRecord(MsgAddRecord msg, long expectedOffset) throws PanaceaApiException, IOException, NoSuchAlgorithmException, InterruptedException, DecoderException {
+    private void testAddRecord(MsgAddRecord msg, long expectedOffset) throws Exception {
         String memo = "add record";
         List<Wallet> groupSignWallets = Arrays.asList(
                 getWallet(TestConst.toMnemonic), getWallet(TestConst.ownerMnemonic));
@@ -142,26 +141,30 @@ public class GrpcAolTest extends AbstractGrpcTest {
         Assert.assertNotNull(response.getTxhash());
         Assert.assertEquals(0, response.getCode());
 
-        String txHash = response.getTxhash();
+        response = pollTxResponse(response.getTxhash(), 10, 1000);
+        TxMsgData txMsgData = TxMsgData.parseFrom(Hex.decodeHex(response.getData()));
+        MsgAddRecordResponse msgRes = MsgAddRecordResponse.parseFrom(txMsgData.getData(0).getData());
+        Assert.assertEquals(expectedOffset, msgRes.getOffset());
+    }
 
-        while (true) {
+    private TxResponse pollTxResponse(String txHash, int maxTries, int sleepMs) throws Exception {
+        // This test code does polling to wait until the tx is included in the block.
+        // But, in real world, pls use more fancy ways such as async jobs.
+        for (int tries = 0; tries < maxTries; tries++) {
             try {
-                response = client.getTxResponse(txHash);
-
-                TxMsgData txMsgData = TxMsgData.parseFrom(Hex.decodeHex(response.getData()));
-                MsgAddRecordResponse msgRes = MsgAddRecordResponse.parseFrom(txMsgData.getData(0).getData());
-                Assert.assertEquals(expectedOffset, msgRes.getOffset());
-                break;
+                return client.getTxResponse(txHash);
             } catch (StatusRuntimeException e) {
                 // if tx was not found (if tx isn't included in the block yet)
                 // Misc: Yeah. I know that the status code INVALID_ARGUMENT isn't proper, but it's the way how Cosmos was implemented.
                 if (e.getStatus().getCode().equals(Status.Code.INVALID_ARGUMENT)) {
-                    Thread.sleep(1000);
+                    Thread.sleep(sleepMs);
                     continue;
                 }
                 throw e;
             }
         }
+
+        throw new Exception("tx not found. maxTries: " + maxTries);
     }
 
     private void testGetTopic(String topicName, long totalWriters, long totalRecords, String description) throws PanaceaApiException {
