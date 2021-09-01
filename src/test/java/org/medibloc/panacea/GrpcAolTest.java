d4ac4044d3070f0d14ec21aaa81cff7d19f23826
Youngjoon Lee <yjlee@medibloc.org>
2021-09-01 17:59:56 +0900

    test: Test AOL AddRecord with the broadcast mode `sync` (#51)

--- src/test/java/org/medibloc/panacea/GrpcAolTest.java
+++ src/test/java/org/medibloc/panacea/GrpcAolTest.java
@@ -1,15 +1,16 @@
 package org.medibloc.panacea;
 
 import com.google.protobuf.ByteString;
+import cosmos.base.abci.v1beta1.TxMsgData;
 import cosmos.base.abci.v1beta1.TxResponse;
 import cosmos.tx.v1beta1.BroadcastMode;
 import cosmos.tx.v1beta1.BroadcastTxRequest;
 import cosmos.tx.v1beta1.Fee;
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
+import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.medibloc.panacea.domain.Coins;
 import org.medibloc.panacea.domain.Transactions;
@@ -23,7 +24,7 @@ import java.util.List;
 public class GrpcAolTest extends AbstractGrpcTest {
 
     @Test
-    public void testAol() throws IOException, NoSuchAlgorithmException, PanaceaApiException {
+    public void testAol() throws Exception {
         Wallet ownerWallet = getWallet(TestConst.ownerMnemonic);
         String ownerAddress = ownerWallet.getAddress();
         Wallet toWallet = getWallet(TestConst.toMnemonic);
@@ -59,7 +60,7 @@ public class GrpcAolTest extends AbstractGrpcTest {
                 .setWriterAddress(ownerAddress)
                 .setFeePayerAddress(toAddress)
                 .build();
-        testAddRecord(addRecordMsg);
+        testAddRecord(addRecordMsg, 0);
         testGetTopic(topicName, 1, 1, createTopicMsg.getDescription());
 
         MsgDeleteWriter deleteWriteMsg = MsgDeleteWriter.newBuilder()
@@ -122,7 +123,7 @@ public class GrpcAolTest extends AbstractGrpcTest {
         Assert.assertEquals(0, response.getCode());
     }
 
-    private void testAddRecord(MsgAddRecord msg) throws PanaceaApiException, IOException, NoSuchAlgorithmException {
+    private void testAddRecord(MsgAddRecord msg, long expectedOffset) throws Exception {
         String memo = "add record";
         List<Wallet> groupSignWallets = Arrays.asList(
                 getWallet(TestConst.toMnemonic), getWallet(TestConst.ownerMnemonic));
@@ -133,12 +134,37 @@ public class GrpcAolTest extends AbstractGrpcTest {
                 msg,
                 memo,
                 fee,
-                BroadcastMode.BROADCAST_MODE_BLOCK);
+                BroadcastMode.BROADCAST_MODE_SYNC);  // Used the sync mode as an example
 
         TxResponse response = client.broadcast(request);
         System.out.println(response.toString());
         Assert.assertNotNull(response.getTxhash());
         Assert.assertEquals(0, response.getCode());
+
+        response = pollTxResponse(response.getTxhash(), 10, 1000);
+        TxMsgData txMsgData = TxMsgData.parseFrom(Hex.decodeHex(response.getData()));
+        MsgAddRecordResponse msgRes = MsgAddRecordResponse.parseFrom(txMsgData.getData(0).getData());
+        Assert.assertEquals(expectedOffset, msgRes.getOffset());
+    }
+
+    private TxResponse pollTxResponse(String txHash, int maxTries, int sleepMs) throws Exception {
+        // This test code does polling to wait until the tx is included in the block.
+        // But, in real world, pls use more fancy ways such as async jobs.
+        for (int tries = 0; tries < maxTries; tries++) {
+            try {
+                return client.getTxResponse(txHash);
+            } catch (StatusRuntimeException e) {
+                // if tx was not found (if tx isn't included in the block yet)
+                // Misc: Yeah. I know that the status code INVALID_ARGUMENT isn't proper, but it's the way how Cosmos was implemented.
+                if (e.getStatus().getCode().equals(Status.Code.INVALID_ARGUMENT)) {
+                    Thread.sleep(sleepMs);
+                    continue;
+                }
+                throw e;
+            }
+        }
+
+        throw new Exception("tx not found. maxTries: " + maxTries);
     }
 
     private void testGetTopic(String topicName, long totalWriters, long totalRecords, String description) throws PanaceaApiException {
