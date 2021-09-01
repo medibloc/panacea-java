db10ad47a0626937c8eaf61d23ec002ccde1d689
Youngjoon Lee <yjlee@medibloc.org>
2021-09-01 16:21:46 +0900

    test: Test AOL AddRecord with the broadcast mode `sync`

--- src/test/java/org/medibloc/panacea/GrpcAolTest.java
+++ src/test/java/org/medibloc/panacea/GrpcAolTest.java
@@ -1,15 +1,17 @@
 package org.medibloc.panacea;
 
 import com.google.protobuf.ByteString;
+import cosmos.base.abci.v1beta1.TxMsgData;
 import cosmos.base.abci.v1beta1.TxResponse;
 import cosmos.tx.v1beta1.BroadcastMode;
 import cosmos.tx.v1beta1.BroadcastTxRequest;
 import cosmos.tx.v1beta1.Fee;
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.medibloc.panacea.domain.Coins;
 import org.medibloc.panacea.domain.Transactions;
@@ -23,7 +25,7 @@ import java.util.List;
 public class GrpcAolTest extends AbstractGrpcTest {
 
     @Test
-    public void testAol() throws IOException, NoSuchAlgorithmException, PanaceaApiException {
+    public void testAol() throws IOException, NoSuchAlgorithmException, PanaceaApiException, InterruptedException, DecoderException {
         Wallet ownerWallet = getWallet(TestConst.ownerMnemonic);
         String ownerAddress = ownerWallet.getAddress();
         Wallet toWallet = getWallet(TestConst.toMnemonic);
@@ -122,7 +124,7 @@ public class GrpcAolTest extends AbstractGrpcTest {
         Assert.assertEquals(0, response.getCode());
     }
 
-    private void testAddRecord(MsgAddRecord msg) throws PanaceaApiException, IOException, NoSuchAlgorithmException {
+    private void testAddRecord(MsgAddRecord msg) throws PanaceaApiException, IOException, NoSuchAlgorithmException, InterruptedException, DecoderException {
         String memo = "add record";
         List<Wallet> groupSignWallets = Arrays.asList(
                 getWallet(TestConst.toMnemonic), getWallet(TestConst.ownerMnemonic));
@@ -133,12 +135,31 @@ public class GrpcAolTest extends AbstractGrpcTest {
                 msg,
                 memo,
                 fee,
-                BroadcastMode.BROADCAST_MODE_BLOCK);
+                BroadcastMode.BROADCAST_MODE_SYNC);  // Used the sync mode as an example
 
         TxResponse response = client.broadcast(request);
         System.out.println(response.toString());
         Assert.assertNotNull(response.getTxhash());
         Assert.assertEquals(0, response.getCode());
+
+        String txHash = response.getTxhash();
+
+        while (true) {
+            try {
+                response = client.getTxResponse(txHash);
+
+                TxMsgData txMsgData = TxMsgData.parseFrom(Hex.decodeHex(response.getData()));
+                MsgAddRecordResponse msgRes = MsgAddRecordResponse.parseFrom(txMsgData.getData(0).getData());
+                System.out.println("offset: " + msgRes.getOffset());
+                break;
+            } catch (StatusRuntimeException e) {
+                if (e.getStatus().getCode().equals(Status.Code.INVALID_ARGUMENT)) { // if tx was not found (if tx isn't included in the block yet)
+                    Thread.sleep(1000);
+                    continue;
+                }
+                throw e;
+            }
+        }
     }
 
     private void testGetTopic(String topicName, long totalWriters, long totalRecords, String description) throws PanaceaApiException {
